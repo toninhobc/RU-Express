c33bc91f8ad52a4e44a31adc878d4d4f9cf53714
Rafael Medeiros <leitao.rma@gmail.com>
2026-07-03 15:03:50 -0300

    feat/query de acessos

--- backend/queries.py
+++ backend/queries.py
@@ -20,14 +20,36 @@ QueryUser = """
         LEFT JOIN Visitante v ON u.id_usuario = v.id_usuario
         WHERE u.id_usuario = %s;
         """
+
 QueryBalance = """
-    SELECT
-        u.id_usuario,
-        u.nome,
-        u.saldo_atual,
-        g.nome_categoria AS categoria
-    FROM Usuario_RU u
-    INNER JOIN Grupo_Acesso g
-        ON u.id_categoria = g.id_categoria
-    WHERE u.id_usuario = %s;
-"""
+        SELECT
+            u.id_usuario,
+            u.nome,
+            u.saldo_atual,
+            g.nome_categoria AS categoria
+        FROM Usuario_RU u
+        INNER JOIN Grupo_Acesso g
+            ON u.id_categoria = g.id_categoria
+        WHERE u.id_usuario = %s;
+        """
+
+QueryAcesso = """
+        SELECT 
+            a.id_acesso,
+            a.data_hora_entrada,
+            a.valor_cobrado,
+            a.peso_prato_kg,
+            c.id_catraca,
+            r.nome_refeitorio,
+            r.tipo_servico,
+            ru.nome_ru AS restaurante
+        FROM Acesso_RU a
+        INNER JOIN Catraca c ON a.id_catraca = c.id_catraca
+        INNER JOIN Refeitorio r ON c.id_refeitorio = r.id_refeitorio
+        INNER JOIN Restaurante_Universitario ru ON r.id_ru = ru.id_ru
+        WHERE a.id_usuario = %s
+        ORDER BY a.data_hora_entrada DESC
+        
+        LIMIT %s OFFSET %s;
+        """
+
